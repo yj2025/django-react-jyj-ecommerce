3922d966e77ce143c007bdc9f6f8a650527b69ad
Fellas <neuneu12@naver.com>
2025-04-23 13:29:09 +0900

    .

--- react-vite-jyj-ecommerce/vite.config.js
+++ react-vite-jyj-ecommerce/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 // https://vite.dev/config/
-// def_1_fruit
+// dev_1_fruit
 export default defineConfig({
   plugins: [react()],
   resolve: {
